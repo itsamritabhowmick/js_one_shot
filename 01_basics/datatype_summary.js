45d2334f20e7ea3aa33a462236f55c547a0248c1
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2025-06-19 22:20:55 +0530

    Summary of all about datatypes

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,46 @@
+/*
+
+const score = 100
+const  scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId)
+
+
+const bigNumber = 345654545313462525622n 
+
+const heroes = ["shaktiman","naagraj","doga"] 
+
+let myobj = {
+    name: "hitesh", 
+    age: 22
+}
+
+*/
+
+// const myFunction = function(){
+//     console.log("hello world")
+// }
+// console.log(typeof myFunction)
+
+//********************************************************
+
+let myYoutubename = "hiteshchoudharydotcom";
+let anothername = myYoutubename;
+anothername = "chaiaurcode ";
+console.log(anothername);
+
+let userOne = {
+  email: "user@googgle.com",
+  upi: "user@ybl",
+};
+
+let userTwo = userOne;
+
+userTwo.email = "hitesh@google.com";
+console.log(userOne.email);
+console.log(userTwo.email);
