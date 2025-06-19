63e703e655292180f9da624612782f97808ae6ff
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2025-06-19 22:11:36 +0530

    all about datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let name = "John Doe";
+let age = 30;
+let isLoggedIn = true;
+
+//number => 2 to power 53
+//bigint
+//string=> ""
+//boolean => true/false
+//null => standalone value
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null); // null is an object in JavaScript
+console.log(typeof undefined); // undefined
