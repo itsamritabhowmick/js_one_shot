70f6084d68f8955573dad16a5b159011679a44a5
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2025-06-19 22:15:47 +0530

    different types of conversion on operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,108 @@
+/*
+let score = 56
+let score1 = "33"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score1) 
+console.log(typeof valueInNumber)
+*/
+
+/* for string to number
+let score2 = "33abc"
+console.log(typeof score2)
+let valueInNumber1 = Number(score2)
+console.log(typeof valueInNumber1)
+console.log(valueInNumber1) // NaN
+*/
+
+/* for null to number
+let score = null
+console.log(typeof score)
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+*/
+
+/* for undefined to number
+let score = undefined;
+console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+*/
+
+/* boolean to number
+
+let score = true
+console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+
+let score1 = false;
+console.log(typeof score1);
+let valueInNumber1 = Number(score1);
+console.log(valueInNumber1);
+
+*/
+/*
+let score = "hitesh";
+console.log(typeof score);
+let value = Number(score);
+console.log(value);
+*/
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+/*
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+*/
+
+/*
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+*/
+
+//*******************Operations*********************
+
+/*
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2**2) // exponentiation
+console.log(2%2) // modulus
+console.log(2**3) // 2^3 = 8
+
+*/
+/*
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+console.log(str3)
+
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + "2")
+console.log("1" + 2 + 2)
+console.log(1+2+"2")
+console.log( ( 3 + 4 ) * 5 % 3) 
+console.log(+true)
+console.log(+"")
+*/
+
+let num1, num2, num3;
+// num1 = num2 = num3 = 2+2
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter); // 101
